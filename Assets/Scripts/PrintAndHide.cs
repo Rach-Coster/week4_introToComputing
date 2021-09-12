b5fb9ff64f78d7617b07f16f44dd12bcd8458592
Rachel Coster <rachel.coster@outlook.com>
2021-09-12 13:21:57 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,18 +6,29 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend; 
 
-
     private int i;
+    private int random; 
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        random = Random.Range(200, 250); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.name + "'s:" + ++i);
+        Debug.Log(gameObject.name + ":" + ++i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if(gameObject.tag == "Blue" && i == random)
+        {
+            rend.enabled = false;
+        }
 
     }
 }
